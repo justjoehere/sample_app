54794d4352f753826cb8e0569e12373d64bb6ee1
Joseph Ours <joseph.ours@gmail.com>
2013-09-28 10:07:33 -0400

    Finish Layout and Routes and Refactor Test and Add User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"
+  root 'static_pages#home'
+  match 'static_pages/help', to: 'static_pages#help', as: 'help', via: 'get'
+  match 'static_pages/home', to: 'static_pages#home', as:'home', via: 'get'
+  match 'static_pages/about', to: 'static_pages#about', as: 'about' ,via: 'get'
+  match 'static_pages/contact', to: 'static_pages#contact', as: 'contact' ,via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
+  #Okay, I've figured out that
+  #match means match the request 'static_pages/help' on a get request.
+  #-->This is the user incoming request!
+  #So http://127.0.0.1:3000/static_pages/help on a 'get' request will be
+  #managed in the to: section. In this case, 'static_pages#help' means
+  #Use the static_pages controller and def help action in the controller
+  #which of course, routes to the help view.
+  #It appears, the 'as:' really is rails magic.  In the views, when it looks for
+  #'help_path' or 'help_url', it is looking for the "as: 'help'"
+  #here to find the right controller/action
+  #
+  #
+  #
+
+  #READ THIS LATER!!!
+  #http://www.dreamincode.net/forums/topic/157760-rails-routes-explained-part-2/
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,25 @@
 require 'spec_helper'
+
 describe "Static pages" do
-  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  #let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  subject {page}
   describe "Help page" do
-    it "should have the h1 Help" do
-      visit '/static_pages/help'
-      page.should have_selector('h1',:text=>'Help')
-    end
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_title("#{base_title} | Help")
-    end
+    before {visit help_path}
+    it {should have_selector('h1',:text=>'Help')}
+    it {should have_title(full_title('Help'))}
     it "should not have a custom page title" do
-      visit '/static_pages/help'
-      assert(page.title!="#{base_title}","Found " + page.title + " looking for mismatch with #{base_title}")
+      assert(page.title!=full_title(''),"Found " + page.title + " looking for mismatch with " + full_title('Help'))
     end
   end
   describe "Home page" do
-    it "should have the h1 Home" do
-      visit '/static_pages/home'
-      page.should have_selector('h1',:text=>'Home')
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_title("#{base_title}")
-    end
+    before {visit home_path}
+    it {should have_selector('h1',:text=>'Home')}
+    it {should have_title(full_title(''))}
     it "should not have a custom page title" do
-      visit '/static_pages/home'
       expect(page).not_to have_title(' | Home')
+    end
+    it "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
     end
   end
   describe "About page" do
@@ -34,23 +27,15 @@ describe "Static pages" do
 #      visit '/static_pages/about'
 #      page.should have_content('About Us')
 #    end
-    it "should have the h1 About Us" do
-      visit '/static_pages/about'
-      page.should have_selector('h1',:text=>'About Us')
-    end
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_title("#{base_title} | About Us")
-    end
+    before {visit about_path}
+    it {should have_selector('h1',:text=>'About Us')}
+    it {should have_title(full_title('About Us'))}
   end
   describe "Contact page" do
-    it "should have the h1 Contact" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1',:text=>'Contact')
-    end
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_title("#{base_title} | Contact")
+    before {visit contact_path}
+    it {should have_title(full_title('Contact'))}
+    it "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
     end
   end
 end
