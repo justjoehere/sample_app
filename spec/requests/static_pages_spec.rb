79756e19faae90feab7d7c23c8d0332dd9697d9e
Joseph Ours <joseph.ours@gmail.com>
2013-09-28 12:40:52 -0400

    Add Link Check Tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,20 @@ describe "Static pages" do
       expect(page).to have_content('Contact')
     end
   end
+  describe "Verify Header and Footer Links" do
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title('About Us'))
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+      click_link "Home"
+      click_link "Sign up now!"
+      expect(page).to have_title(full_title('Sign up'))
+      click_link "sample app"
+      expect(page).to have_title(full_title(''))
+    end
+  end
 end
