f5cb3a19110ded40ab16dddf4ea24fafbfd8b292
Joseph Ours <joseph.ours@gmail.com>
2013-09-30 13:47:49 -0400

    Add notes to user model class

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }#ensures uniquness via same case inserts
 
+  #attr_accessor :name, :email
+  #for some reason the preceeding line causes an inability to save.  Not sure if this is ruby magic
+  #where I've declared the name, email fields in the db creates issues when
+  #you explicitly state them in this model class
+
 end
